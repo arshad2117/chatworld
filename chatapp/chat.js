68dbeaf5a0e1e8940ca0dd5180997d0a8b046c67
arshad <arshad2117@gmail.com>
2017-12-19 16:03:08 +0530

    Add files via upload

---
+++ chatapp/chat.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname+"/fend.html");
+});
+
+io.on('connection',function(socket){
+	console.log('A user was connected');
+	socket.on('sent',function(user,messg){
+		console.log("A message was received");
+		io.emit('gotmsg',user,messg);
+	});
+});
+
+http.listen(3000, function(){
+	console.log("SERVER IS RUNNING");	
+});
+    
